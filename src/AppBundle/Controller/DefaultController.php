fee59488ea6a60eca8861a06744a1661754e5b56
Julio López <julio.lopez@mo2o.com>
2017-04-26 14:07:11 +0200

    feat: Update main behaviour in order to use the new classes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,37 +2,104 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\entityAPI;
+use AppBundle\Entity\Matrix;
+use AppBundle\Entity\MatrixException;
+use AppBundle\Entity\Square;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     const COORDINATE_X = 2;
     const COORDINATE_Y = 2;
     const RANGE = 1;
-    const HEIGHT_WIDTH = 5;
+    const SIDE = 5;
+
+    private $coordinateX;
+    private $coordinateY;
+    private $range;
+    private $side;
 
     /**
+     * @var Request $request
+     * @return Response
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        $entityAPI = entityAPI::createMatrixForTestWithHeightAndWidth(self::HEIGHT_WIDTH);
+        $parameters = $this->loadParameters($request->query->all());
 
-        $subMatrix = $entityAPI->getSubMatrixFromCoordinatesAndRange(
-            self::COORDINATE_X,
-            self::COORDINATE_Y,
-            self::RANGE);
+        $this->checkParameters();
 
-        $matrix = $entityAPI->getMatrix();
+        $matrix = $this->createMatrix();
 
-        // replace this example code with whatever you need
+        $subMatrix = $matrix->getSubMatrixFromCoordinatesAndRange(
+            $this->coordinateX,
+            $this->coordinateY,
+            $this->range);
+
+        [$matrixTwig, $subMatrixTwig] = $this->prepareForTwig($matrix, $subMatrix);
+
         return $this->render('default/index.html.twig', [
-            'matrix' => $matrix,
-            'subMatrix' => $subMatrix,
-            'range' => self::RANGE,
-            'width_matrix' => self::HEIGHT_WIDTH
+            'matrix' => $matrixTwig,
+            'subMatrix' => $subMatrixTwig,
+            'range' => $this->range,
+            'width_matrix' => $this->side,
+            'parameters' => $parameters
         ]);
+    }
+
+    private function checkParameters()
+    {
+        $matrixException = MatrixException::createWithSideAndRange($this->side, $this->range);
+        $square = Square::createWithCoordinates($this->coordinateX, $this->coordinateY);
+
+        $matrixException->setMiddleSquare($square);
+
+        $matrixException->check();
+    }
+
+    private function createMatrix(): Matrix
+    {
+        $squares = Matrix::createSquares($this->side);
+
+        return Matrix::createWithSideAndSquares($this->side, $squares);
+    }
+
+
+    private function prepareForTwig(Matrix $matrix, Matrix $subMatrix): array
+    {
+        $matrixTwig = $matrix->prepareForTwig();
+
+        $subMatrixTwig = $subMatrix->prepareForTwig();
+        $subMatrixTwig = array_merge(...$subMatrixTwig);
+
+        return [$matrixTwig, $subMatrixTwig];
+    }
+
+    private function loadParameters(array $parameters): array
+    {
+        if (empty($parameters)) {
+            $this->coordinateX = self::COORDINATE_X;
+            $this->coordinateY = self::COORDINATE_Y;
+            $this->side = self::SIDE;
+            $this->range = self::RANGE;
+
+            $parameters = [
+                'coordinateX' => '',
+                'coordinateY' => '',
+                'side' => '',
+                'range' => ''
+            ];
+        } else {
+            $this->coordinateX = $parameters['coordinateX'];
+            $this->coordinateY = $parameters['coordinateY'];
+            $this->side = $parameters['side'];
+            $this->range = $parameters['range'];
+        }
+
+        return $parameters;
     }
 }
